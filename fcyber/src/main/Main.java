6b41170d606d70c9c9f9f0109a5d922a4b5572fd
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-01-20 17:58:58 +0300

    notHesaplamaBasitJava
    basit bir not hesaplama programı geliştirdim

---
+++ fcyber/src/main/Main.java
@@ -0,0 +1,39 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// MEEBBBBBB(TR)
+		System.out.println("*******************************************************");
+		System.out.println("------------  Matamatik 13.sýnýf notlarý  ------------");
+		System.out.println("-------------------------------------------------------");
+
+		int point0 = 32; // not1
+		int point1 =42; // not2
+		int toplam = point0 + point1;
+		int kalan = toplam / 2;
+
+		if (kalan > 50) {
+
+			System.out.println("          ------TEBRÝKLER------");
+
+			System.out.println("             Geçtiðin not=" + kalan);                    //normalde programý süslemem ama olsun dedimmm
+			System.out.println("--------------------------------------------------------");
+		} else {
+
+			System.out.println("       -----SENEYE BE KNK ÜZÜLME-----");
+			System.out.println("           Kaldýðýn not=" + kalan);
+			System.out.println("------------------------------------------------------");
+		}
+
+		/*
+		 * 
+		 * 
+		 * burda basit bir þekilde if ve else kullandým Basit bir programlama 20.01.21
+		 * attýðým tarih
+		 * 
+		 * 
+		 * 
+		 */
+	}
+}
