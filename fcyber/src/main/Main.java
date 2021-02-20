8ff8750dace494d8d569ad37ed00d75c1d145101
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-21 00:11:10 +0300

    notHesaplama

--- fcyber/src/main/Main.java
+++ fcyber/src/main/Main.java
@@ -3,10 +3,11 @@ package main;
 import java.util.Scanner;
 
 public class Main {
-
+	//Normalde 3 not vardi babam sayesinde 8 e kadar cıkardim.Saygilar Mustafa abi
 	public static void main(String[] args) {
+
 		Scanner giris = new Scanner(System.in);
-		double not1, not2, not3;
+		double not1, not2, not3, not4, not5, not6, not7, not8;
 		double total;
 		String sonuc;
 
@@ -17,9 +18,19 @@ public class Main {
 		not2 = giris.nextDouble();
 		System.out.println("3.notu giriniz");
 		not3 = giris.nextDouble();
+		System.out.println("4.notu giriniz");
+		not4 = giris.nextDouble();
+		System.out.println("5.notu giriniz");
+		not5 = giris.nextDouble();
+		System.out.println("6.notu giriniz");
+		not6 = giris.nextDouble();
+		System.out.println("7.notu giriniz");
+		not7 = giris.nextDouble();
+		System.out.println("8.notu giriniz");
+		not8 = giris.nextDouble();
 
-		total = not1 + not2 + not3;
-		total /= 3;
+		total = not1 + not2 + not3 + not4 + not5 + not6 + not7 + not8;
+		total /= 8;
 
 		if (total < 45) {
 			sonuc = "Zayif";
